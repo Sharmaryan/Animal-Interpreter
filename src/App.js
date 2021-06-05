d2dcf82984f0809e83cc0c540bfe7a3e56187a38
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-06-05 15:29:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./styles.css";
+import { useState } from "react";
+
+const flags = {
+  "🐺": "wolf",
+  "🦊": "fox",
+  "🐅": "tiger",
+  "🐆": "leopard",
+  "🦄": "unicorn",
+  "🐼": "panda",
+  "🐄": "cow"
+};
+
+let emojisWeKnow = Object.keys(flags);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+
+  function inputHandler(event) {
+    let currentEmoji = event.target.value;
+    let searchedEmoji = flags[currentEmoji];
+    if (searchedEmoji === undefined) {
+      searchedEmoji = "sorry, we don't have this in our database";
+    }
+    setEmoji(searchedEmoji);
+  }
+  function clickHandler(emoji) {
+    let emojiName = flags[emoji];
+    setEmoji(emojiName);
+  }
+  return (
+    <div className="App">
+      <h1>Animals Interpreter</h1>
+      <input onChange={inputHandler}></input>
+      <h2>{emoji}</h2>
+      <h3>emojis we know </h3>
+      {emojisWeKnow.map((emoji) => (
+        <span
+          style={{ fontSize: "1.5rem", padding: ".5rem", cursor: "pointer" }}
+          onClick={() => clickHandler(emoji)}
+          key={emoji}
+        >
+          {emoji}
+        </span>
+      ))}
+    </div>
+  );
+}
